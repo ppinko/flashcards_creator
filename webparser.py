7caac0222bb6f139667aebbdf0544c670c978a79
ppinko <p.pinkowicz@gmail.com>
2021-05-21 17:39:52 +0200

    IMPLEMENT - Add class for flashcards representation

--- webparser.py
+++ webparser.py
@@ -8,6 +8,38 @@ basicUrl = 'https://www.linguee.de/deutsch-englisch/search?source=englisch&query
 
 words = ['home', 'kitchen', 'bedroom', 'bathroom', 'corridor']
 translated = []
+flashcards = []
+
+class Flashcard:
+    # data members
+    word = ''
+    translations = []
+    questions = []
+    examples = []
+
+    def __init__(self, word):
+        self.word = word
+
+    def print(self):
+        print('#' * 20)
+        print(self.word, '\n')
+        for i, val in enumerate(self.questions):
+            if val != '':
+                print('{0}) {1}'.format(i, val))
+        print()
+        
+        equivalents = ''
+        for i in self.translations:
+            equivalents += i + ', '
+        equivalents = equivalents[0:-2]
+        print(equivalents, '\n')
+
+        for i, val in enumerate(self.examples):
+            if val != '':
+                print('{0}) {1} -> {2}'.format(i, self.translations[i], val))
+            else:
+                print('{0}) {1}'.format(i, self.translations[i]))
+
 
 for i in range(len(words)):
     url = basicUrl + words[i]
