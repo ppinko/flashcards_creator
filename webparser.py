0754778b4c7bc35a238d213e715e3f779df97c7e
ppinko <p.pinkowicz@gmail.com>
2021-05-21 21:23:17 +0200

    REFACTORING - Remove print statement and add front and back methods

--- webparser.py
+++ webparser.py
@@ -46,31 +46,51 @@ class Flashcard:
             else:
                 print('{0}) {1}'.format(i, self.translations[i]))
 
+    def front(self) -> str:
+        front = ''
+        front += self.word + '<br/><br/>'
+        for i, val in enumerate(self.questions):
+            if val != '':
+                front += '{0}) {1}'.format(i, val) + '<br/>'
+        return front
+
+    def back(self) -> str:
+        equivalents = ''
+        for i in self.translations:
+            equivalents += i + ', '
+        equivalents = equivalents[0:-2]
+
+        back = equivalents + '<br/><br/>'
+        for i, val in enumerate(self.examples):
+            if val != '':
+                back += '{0}) {1} -> {2}'.format(i,
+                                                 self.translations[i],
+                                                 val) + '<br/>'
+            else:
+                back += '{0}) {1}'.format(i, self.translations[i]) + '<br/>'
+        return back
+
 
 for i, word in enumerate(words):
     card = Flashcard(word)
     url = basicUrl + word
-    # print(url)
     page = requests.get(url)
     soup = bs4(page.text, 'html.parser')
     divs = soup.findAll('div', class_='translation sortablemg featured')
     for i, div in enumerate(divs):
         if i < 3:
             translation = div.find('a', class_='dictLink featured')
-            # print(translation.getText())
             card.translations.append(translation.getText())
 
             example = div.find('div', class_='example line')
             examplesENG = example.findAll('span', class_='tag_s')
             if len(examplesENG) != 0:
-                # print(examplesENG[0].getText())
                 card.questions.append(examplesENG[0].getText())
             else:
                 card.questions.append('')
 
             examplesDEU = example.findAll('span', class_='tag_t')
             if len(examplesDEU) != 0:
-                # print(examplesDEU[0].getText())
                 card.examples.append(examplesDEU[0].getText())
             else:
                 card.examples.append('')
@@ -105,22 +125,15 @@ basicAndReversedEngDeu = genanki.Model(
     css='.card {\n font-family: arial;\n font-size: 20px;\n text-align: center;\n color: black;\n background-color: white;\n}\n',
 )
 
-
 my_deck = genanki.Deck(
     2059400111,
-    'roomies')
-
-my_note = genanki.Note(
-    model=basicAndReversedEngDeu,
-    fields=['Capital of Argentina', 'Buenos Aires'])
-
-my_deck.add_note(my_note)
+    'rooms')
 
 for i in flashcards:
     my_note = genanki.Note(
         model=basicAndReversedEngDeu,
-        fields=[i.word, i.translations[0]])
+        fields=[i.front(), i.back()])
     my_deck.add_note(my_note)
-    # i.print()
 
+# generate Anki deck
 genanki.Package(my_deck).write_to_file('rooms.apkg')
