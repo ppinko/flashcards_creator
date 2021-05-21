7ba35d25e9f5beff8da264f5f30e20f8e4697fae
ppinko <p.pinkowicz@gmail.com>
2021-05-21 18:40:45 +0200

    IMPLEMENT - Implement first version of the class Flashcard

--- webparser.py
+++ webparser.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup as bs4
 import requests
 import pandas as pd
+import copy
 
 print('\n')
 
@@ -9,6 +10,7 @@ basicUrl = 'https://www.linguee.de/deutsch-englisch/search?source=englisch&query
 words = ['home', 'kitchen', 'bedroom', 'bathroom', 'corridor']
 translated = []
 flashcards = []
+
 
 class Flashcard:
     # data members
@@ -19,6 +21,9 @@ class Flashcard:
 
     def __init__(self, word):
         self.word = word
+        self.translations = []
+        self.questions = []
+        self.examples = []
 
     def print(self):
         print('#' * 20)
@@ -27,7 +32,7 @@ class Flashcard:
             if val != '':
                 print('{0}) {1}'.format(i, val))
         print()
-        
+
         equivalents = ''
         for i in self.translations:
             equivalents += i + ', '
@@ -41,23 +46,35 @@ class Flashcard:
                 print('{0}) {1}'.format(i, self.translations[i]))
 
 
-for i in range(len(words)):
-    url = basicUrl + words[i]
+for i, word in enumerate(words):
+    card = Flashcard(word)
+    url = basicUrl + word
     # print(url)
-    page = requests.get(url)               
+    page = requests.get(url)
     soup = bs4(page.text, 'html.parser')
-    divs =soup.findAll('div', class_='translation sortablemg featured')
+    divs = soup.findAll('div', class_='translation sortablemg featured')
     for i, div in enumerate(divs):
         if i < 3:
-            translation=div.find('a',class_='dictLink featured')
-            print(translation.getText())
-            example = div.find('div', class_='example line')
+            translation = div.find('a', class_='dictLink featured')
+            # print(translation.getText())
+            card.translations.append(translation.getText())
 
+            example = div.find('div', class_='example line')
             examplesENG = example.findAll('span', class_='tag_s')
             if len(examplesENG) != 0:
-                print(examplesENG[0].getText()) 
+                # print(examplesENG[0].getText())
+                card.questions.append(examplesENG[0].getText())
+            else:
+                card.questions.append('')
 
             examplesDEU = example.findAll('span', class_='tag_t')
             if len(examplesDEU) != 0:
-                print(examplesDEU[0].getText()) 
-    print()
+                # print(examplesDEU[0].getText())
+                card.examples.append(examplesDEU[0].getText())
+            else:
+                card.examples.append('')
+
+    flashcards.append(copy.deepcopy(card))
+
+for i in flashcards:
+    i.print()
